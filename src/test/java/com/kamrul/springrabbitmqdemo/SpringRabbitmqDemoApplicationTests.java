ec916d3b619ad6965fde417c418a6a342bc83a2c
kamrulbari14 <kamrulbpriyo0523@gmail.com>
2023-06-18 01:28:48 +0600

    rabbitmq configuration

---
+++ src/test/java/com/kamrul/springrabbitmqdemo/SpringRabbitmqDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kamrul.springrabbitmqdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitmqDemoApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
